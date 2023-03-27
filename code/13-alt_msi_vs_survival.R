f99381d503bf78fe87563da60850a03c09cf93ac
komalsrathi <komal.rathi@gmail.com>
2023-03-27 10:40:30 -0400

    update surv plots

--- code/13-alt_msi_vs_survival.R
+++ code/13-alt_msi_vs_survival.R
@@ -24,36 +24,41 @@ clin_df <- clin_df %>%
 # add to ALT and MSI output
 surv_data <- merged_output %>%
   inner_join(clin_df, by = c("sample_id" = "Sample_ID"))
+surv_data$ALT_status <- factor(surv_data$ALT_status, levels = c("ALT+", "ALT-"))
 
 # survival curves stratified by ALT status 
 fit <- survival::survfit(Surv(as.numeric(OS_days), OS_status) ~ ALT_status, data = surv_data)
+pvalue <- round(survdiff(Surv(as.numeric(OS_days), OS_status) ~ ALT_status, data = surv_data)$p, 3)
 pdf(file = file.path(output_dir, "alt_vs_survival.pdf"), height = 8, width = 10, onefile = FALSE)
-p <- ggsurvplot(fit,
-                title = "ALT Status",
-                data = surv_data,
-                font.x = c(12, face = "bold"),
-                font.tickslab = c(10, face = "bold"),
-                font.y = c(12, face = "bold"),
-                font.legend = c(10, "bold"),
-                pval = TRUE, 
-                pval.method = TRUE,
+p <- ggsurvplot(fit, 
+                title = "ALT Status vs Survival",
+                data = surv_data, 
+                font.x = c(20, face = "bold"),
+                font.tickslab = c(20),
+                font.y = c(20, face = "bold"),
+                font.legend = c(20, "bold"),
+                pval = FALSE, 
+                pval.method = FALSE,
                 ggtheme = theme_minimal(),
                 linetype = "strata",
                 legend = "bottom")  +
   guides(colour = guide_legend(ncol = 2)) 
+p <- p$plot +
+  annotate("text", x = 12000, y = 0.75, label = paste0("Log-rank\nP-value: ", pvalue), cex=6, col="black", vjust=0, hjust = 1.1, fontface=1)
 print(p)
 dev.off()
 
 # survival curves stratified by MSI (High > 3.5 and Low) 
+surv_data$Type <- ifelse(surv_data$MSI_Percent > (surv_data$MSI_Percent %>% mean), TRUE, FALSE)
 fit <- survival::survfit(Surv(as.numeric(OS_days), OS_status) ~ Type, data = surv_data)
 pdf(file = file.path(output_dir, "msi_vs_survival.pdf"), height = 8, width = 10, onefile = FALSE)
 p <- ggsurvplot(fit,
-                title = "MSI",
+                title = "MSI vs Survival",
                 data = surv_data,
-                font.x = c(12, face = "bold"),
+                font.x = c(14, face = "bold"),
                 font.tickslab = c(10, face = "bold"),
-                font.y = c(12, face = "bold"),
-                font.legend = c(10, "bold"),
+                font.y = c(14, face = "bold"),
+                font.legend = c(12, "bold"),
                 pval = TRUE, 
                 pval.method = TRUE,
                 ggtheme = theme_minimal(),
