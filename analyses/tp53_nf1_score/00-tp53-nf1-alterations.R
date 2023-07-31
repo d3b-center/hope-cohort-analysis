ccf3e8c8755047b48185bca428b0a9a3167f4a12
Zhuangzhuang Geng <zz.geng@hotmail.com>
2023-07-31 14:18:10 -0400

    add tp53 score module

---
+++ analyses/tp53_nf1_score/00-tp53-nf1-alterations.R
@@ -0,0 +1,179 @@
+# Author: Krutika Gaonkar
+#
+# Read in consensus snv calls to gather alterations in TP53 and NF1
+# to evaluate classifier
+# @params snvConsensus multi-caller consensus snv calls
+# @params cnvConsensus multi-caller consensus cnv calls
+# @params histologyFile histology file: histologies.tsv
+# @params outputFolder output folder for alteration file
+# @params gencode cds bed file from gencode
+
+suppressPackageStartupMessages(library("optparse"))
+suppressPackageStartupMessages(library("tidyverse"))
+suppressPackageStartupMessages(library("readr"))
+suppressPackageStartupMessages(library("GenomicRanges"))
+
+#### Source functions ----------------------------------------------------------
+# We can use functions from the `snv-callers` module of the OpenPedCan project
+# TODO: if a common util folder is established, use that instead
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+scratch_dir <- file.path(root_dir, "scratch")
+
+source(file.path(root_dir, 
+                 "analyses", 
+                 "until",
+                 "tmb_functions.R"))
+
+#### Parse command line options ------------------------------------------------
+
+option_list <- list(
+  make_option(c("-s", "--snvConsensus"),type="character",
+              help="Consensus snv calls (.tsv) "),
+  make_option(c("-c","--cnvConsensus"),type="character",
+               help="consensus cnv calls (.tsv) "),
+  make_option(c("-e","--expr"),type="character",
+              help="rna expression tpm or fpkm (.rds) "),
+  make_option(c("-h","--histologyFile"),type="character",
+              help="histology file for all samples (.tsv)"),
+  make_option(c("-o","--outputFolder"),type="character",
+              help="output folder for results "),
+  make_option(c("-g","--gencode"),type="character",
+              help="cds gencode bed file"),
+  make_option(c("-r","--cohort"),type="character",
+              help="list of cohorts to subset the files to (.tsv)")
+)
+
+opt <- parse_args(OptionParser(option_list=option_list,add_help_option = FALSE))
+snvConsensusFile <- opt$snvConsensus
+snvConsensusFile <- opt$snvConsensus
+expFile <- opt$expr
+histologyFile <- opt$histologyFile
+outputFolder <- opt$outputFolder
+gencodeBed <- opt$gencode
+cnvConsensusFile <- opt$cnvConsensus
+cohort_interest<-unlist(strsplit(opt$cohort,","))
+
+if(!dir.exists(outputFolder)){
+  dir.create(outputFolder)
+}
+
+#### Generate files with TP53, NF1 mutations -----------------------------------
+
+# read in consensus SNV files
+consensus_snv <- readr::read_rds(snvConsensusFile) %>% 
+  as.data.frame() %>%
+  select("Chromosome",
+         "Start_Position",
+         "End_Position",
+         "Strand",
+         "Variant_Classification",
+         "Kids_First_Biospecimen_ID",
+         "Hugo_Symbol")
+
+# read in consensus CNV file
+cnvConsensus <- readr::read_rds(cnvConsensusFile) %>%
+  as.data.frame() %>%
+  dplyr::filter(!grepl('chrX|chrY', chromosome)) %>%
+  dplyr::select(hgnc_symbol,
+                Kids_First_Biospecimen_ID,
+                status)
+
+
+# gencode cds region BED file
+gencode_cds <- read_tsv(gencodeBed, col_names = FALSE)
+
+# histology file
+histology <- read_tsv(histologyFile, guess_max = 100000)
+
+# filter the MAF data.frame to only include entries that fall within the
+# CDS bed file regions
+coding_consensus_snv <- snv_ranges_filter(maf_df = consensus_snv,
+                                          keep_ranges = gencode_cds)
+
+# subset to TP53, removing silent mutations and mutations in introns
+tp53_coding <- coding_consensus_snv %>%
+  filter(Hugo_Symbol == "TP53") %>%
+  filter(!(Variant_Classification %in% c("Silent", "Intron")))
+
+# subset to TP53 cnv loss and format to tp53_coding file format
+tp53_loss<-cnvConsensus %>% 
+  filter(hgnc_symbol == "TP53",
+         status == "loss") %>%
+  rename("Kids_First_Biospecimen_ID" = "Tumor_Sample_Barcode",
+         "status" = "Variant_Classification",
+         "hgnc_symbol" = "Hugo_Symbol")
+
+# subset to NF1, removing silent mutations, mutations in introns, and missense
+# mutations -- we exclude missense mutations because they are not annotated
+# with OncoKB
+# https://github.com/AlexsLemonade/OpenPBTA-analysis/pull/381#issuecomment-570748578
+nf1_coding <- coding_consensus_snv %>%
+  filter(Hugo_Symbol == "NF1") %>%
+  filter(!(Variant_Classification %in% c("Silent",
+                                         "Intron",
+                                         "Missense_Mutation")))
+
+# subset to NF1 loss and format to nf1_coding file format
+nf1_loss<-cnvConsensus %>% 
+  filter(hgnc_symbol == "NF1",
+         status == "loss") %>%
+  rename("Kids_First_Biospecimen_ID" = "Tumor_Sample_Barcode",
+         "status" = "Variant_Classification",
+         "hgnc_symbol" = "Hugo_Symbol")
+
+# include only the relevant columns from the MAF file and merge cnv loss dataframes as well
+tp53_nf1_coding <- tp53_coding %>%
+  bind_rows(tp53_loss,nf1_coding,nf1_loss)
+
+# biospecimen IDs for tumor DNA-seq
+bs_ids <- histology %>%
+  filter(sample_type == "Tumor",
+         experimental_strategy != "RNA-Seq") %>%
+  filter(cohort %in% cohort_interest) %>%
+  pull(Kids_First_Biospecimen_ID)
+
+
+# all BS ids that are not in the data frame that contain the TP53 and NF1
+# coding mutations should be labeled as not having either
+bs_ids_without_mut <- setdiff(bs_ids,
+                              unique(tp53_nf1_coding$Tumor_Sample_Barcode))
+
+# add the TP53 and NF1 wildtype samples into the data.frame
+tp53_nf1_coding <- bind_rows(tp53_nf1_coding,
+                             data.frame(
+                               Tumor_Sample_Barcode = bs_ids_without_mut,
+                               Hugo_Symbol = "No_TP53_NF1_alt")
+                             )
+
+# save TP53 and NF1 SNV alterations
+write_tsv(tp53_nf1_coding,
+          file.path(outputFolder, "TP53_NF1_snv_alteration.tsv"))
+
+# read in expression RDS file
+rna <- readRDS(expFile)
+
+# subset hist for those in rna matrix
+hist_rna <- histology %>%
+  filter(Kids_First_Biospecimen_ID %in% names(rna),
+         cohort %in% cohort_interest) %>%
+  mutate(RNA_library = case_when(RNA_library == "poly-A stranded" ~ "poly-A-stranded",
+                                 TRUE ~ as.character(RNA_library)))
+
+# prepare rna-seq files
+library_list <- hist_rna %>%
+  pull(RNA_library) %>%
+  unique()
+
+# subset RNA file by library type
+for (each in library_list){
+  
+  bs_ids <- hist_rna %>%
+    filter(RNA_library == each) %>%
+    pull(Kids_First_Biospecimen_ID)
+  
+  exp_subset <- rna[,bs_ids]
+  
+  write_rds(exp_subset, file.path(scratch_dir, 
+                                  paste0("gene-expression-rsem-tpm-collapsed-", each, ".rds"))
+  )
+}
---
+++ analyses/tp53_nf1_score/README.md
@@ -0,0 +1,75 @@
+## Apply classifiers trained on TCGA RNA-seq data
+
+**Module author:** Krutika Gaonkar ([@kgaonkar6](https://github.com/kgaonkar6)) and Jaclyn Taroni (@jaclyn-taroni); code adapted from Gregory Way ([@gwaygenomics](https://github.com/gwaygenomics))
+
+This module is adapted from: [`marislab/pdx-classification`](https://github.com/marislab/pdx-classification).
+Now published in [Rokita et al. _Cell Reports._ 2019.](https://doi.org/10.1016/j.celrep.2019.09.071)
+
+In brief, _TP53_ inactivation, _NF1_ inactivation, and Ras activation classifiers are applied to the OpenPedCan RNA-seq data.
+The classifiers were trained on TCGA PanCan data ([Way et al. _Cell Reports._ 2018](https://doi.org/10.1016/j.celrep.2018.03.046), [Knijnenburg et al. _Cell Reports._ 2018.](https://doi.org/10.1016/j.celrep.2018.03.076)).
+See [`01-apply-classifier.py`](01-apply-classifier.py) for more information about the procedure.
+To evaluate the classifier scores, we use [`02-evaluate-classifier.py`](02-evaluate-classifier.py) and input SNV data to identify true TP53/NF1 loss samples and compare scores of shuffled data to true calls and plot ROC curves. 
+
+#### Running the analysis
+
+The analysis can be run with the following (assuming you are in the analysis directory):
+
+```
+bash run_classifier.sh
+```
+
+#### Inputs from data download
+
+`snv-consensus-plus-hotspots.maf.tsv.gz`: from consensus SNV calls that are present in all 3 callers (strelka2,mutect2 and lancet) plus hotspot rescued
+`gene-expression-rsem-tpm-collapsed.rds` : TPM values per gene in biospecimen_id
+`consensus_seg_with_status.tsv` : created by `analyses/focal-cn-file-preparation/02-add-ploidy-consensus.Rmd` - available through data download
+`cnvkit_with_status.tsv`: created by `analyses/focal-cn-file-preparation/01-add-ploidy-cnvkit.Rmd` -available through data download
+`sv-manta.tsv.gz` : Structural Variants called by manta
+
+### Order of analysis
+
+`00-tp53-nf1-alterations.R` produces `TP53_NF1_snv_alteration.tsv`, which contains information about the presence or absence of coding SNVs in _TP53_ and _NF1_ for the purpose of evaluating the classifier results.
+For evaluation purposes, a coding SNV 1) is found in a CDS region and 2) is not a silent mutation or in an intron as indicated by the `Variant_Classification` column of the consensus mutation file.
+_NF1_ positive examples are additionally filtered to remove missense mutations, as these are not annotated with OncoKB ([#381 (comment)](https://github.com/AlexsLemonade/OpenPBTA-analysis/pull/381#issuecomment-570748578)).
+
+`01-apply-classifier.py` produces  `results/gene-expression-rsem-fpkm-collapsed.stranded_classifier_scores.tsv`, which contains all 3 classifier scores for all RNA data and for shuffled RNA (e.g., random) data.
+- output file of this notebook: `TP53_NF1_snv_alteration.tsv`
+
+`02-qc-rna_expression_score.Rmd` here expression of TP53 gene was compared to TP53 classifier score. This script runs comparison for all different types of RNA library in the datasets. Currently, two RNA library type, `stranded` and `polya stranded` generated strong negative correlation between TP53 expression and TP53 inactivation score, while `polya` samples do not seem to have strong correlation. Thus, for `stranded` and `polya stranded`, expression and classifier score together might be able to predict function; however, for polya, the correlation is not statistically significant.
+- output file of this notebook: `gene-expression-rsem-tpm-collapsed_classifier_scores.tsv`
+
+`03-tp53-cnv-loss-domain.Rmd` here copy_number of regions overlapping TP53 functional domains were compared to TP53 classifier score. We find tumors with TP53 copies <=1 have higher TP53 classifier scores, so we only retain biospecimens with <= 1 copy TP53 as high confidence TP53 loss. **NOTE**: since now WGS and WXS have their CNV calls processed different (WGS uses 2/3 consensus workflow and WXS only uses WXS samples) - the inputs were read-in, subsetted and then merged for analyses.
+- output file of this notebook: `loss_overlap_domains_tp53.tsv`
+
+`04-tp53-sv-loss.Rmd` here structural variant breakpoints overlapping TP53 are investigated for CNV loss or low expression to gather high confidence TP53 loss via Structural Variants. 
+- output files of this notebook: `fusion_bk_tp53_loss.tsv` and `sv_overlap_tp53.tsv`
+
+`05-tp53-altered-annotation.Rmd` here we take a deeper look into tp53 altered status with respect to number of SNVs/CNVs suggesting bi-allelic mutations or with respect to cancer_predisposition and tp53 classifier scores.
+- output file of this notebook: `tp53_altered_status.tsv`
+
+Columns | Description
+-- | --
+sample_id	| 7316-XXXX id used to match DNA and RNA Kids_First_Biospecimen_IDs
+Kids_First_Biospecimen_ID_DNA	| Associated DNA Kids_First_Biospecimen_IDs
+Kids_First_Biospecimen_ID_RNA	| Associated RNA Kids_First_Biospecimen_IDs
+cancer_predispositions	| Germline cancer predisposition status
+tp53_score	| TP53 loss classifier score
+SNV_indel_counts	| Number of deleterious SNVs found in DNA sample
+CNV_loss_counts	| Number of CNV losses found in DNA sample
+HGVSp_Short	| Short format of protein level change used as SNV evidence 
+CNV_loss_evidence | copy_number of CNV overlapping functional domains of TP53 used as evidence 	
+hotspot	| Any 1 SNV shown in HGVSp_Short overlaps MSKCC cancer hotspot [database](https://www.cancerhotspots.org/#/home)
+activating	| Any 1 SNV shown in HGVSp_Short overlaps TP53_ activating mutations R273C and R248W. [Reference](https://pubmed.ncbi.nlm.nih.gov/17417627/) and [reference](https://pubmed.ncbi.nlm.nih.gov/24677579/). 
+tp53_altered | Combined evidence, cancer predisposition and score based tp53 status
+
+
+`06-evaluate-classifier.py` evaluates classifier score with TP53 alterations (non-synonymous SNV and all status == "loss" in consensus CNV file from 00-tp53-nf1-alterations.R) 
+
+Because some of the classifier genes are not present in the OpenPedCan dataset, the scores should be interpreted as continuous values representing relative gene alterations and not as probabilities.
+
+ROC curve for TP53 classifier scores are saved in the results folder. We iterate through all possible RNA library types and print out the graphs accordingly. With TARGET samples, we have 3 RNA library types and 3 plots were generated:
+`poly-A_TP53.png`
+`stranded_TP53.png`
+`exome_capture_TP53.png`
+
+
