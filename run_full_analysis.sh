09443542607127500cc24d826a704af1578a3048
komalsrathi <komal.rathi@gmail.com>
2023-08-07 09:16:59 -0400

    remove old folders

--- run_full_analysis.sh
+++ run_full_analysis.sh
@@ -1,30 +1,17 @@
 # create data availability plots
-bash run_data_plots.sh
+bash analyses/data-availability/run_data_plots.sh
 
-# create master annotation
-Rscript --vanilla code/02-create-master-annotation.R
+# create merged files
+bash analyses/merge-files/run_merge_files.sh
 
-# MSI analysis
-bash run_msisensor_analysis.sh
-
-# oncoplots
-bash run_oncoplots.sh
-
-# misc analysis
-# gene alterations correlation
-Rscript --vanilla code/08-gene_alteration_correlation.R
-
-# MSI vs MMR pathways
-Rscript --vanilla code/09-msi_vs_mmr_pathways.R
-
-# ALT vs other variables
-Rscript --vanilla code/10-correlation_alt_vs_vars.R
-
-# cascade plots
-Rscript --vanilla code/11-cascade_plots.R
-
-# major SNV correlations
-Rscript --vanilla code/12-major_snv_analysis.R
+# create oncoplots
+bash analyses/oncoplots/run_oncoplots.sh
 
 # survival analysis
-Rscript --vanilla code/13-survival-analysis.R
+Rscript --vanilla analyses/survival-analysis/01-survival-analysis.R
+
+# ALT correlations
+Rscript --vanilla analyses/alt-analysis/01-correlation_alt_vs_vars.R
+
+# MSIsensor-pro analysis
+bash analyses/msi-sensor-analysis/run_msisensor_analysis.sh
