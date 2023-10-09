f017fb2ba5adbc6d6277c275654d2a6c7eecb5b1
komalsrathi <komal.rathi@gmail.com>
2023-10-09 16:42:36 -0400

    move MSI merged files to MSI analysis module

--- analyses/merge-files/01-create_merged_files.R
+++ analyses/merge-files/01-create_merged_files.R
@@ -161,33 +161,6 @@ hope_cohort_cnv <- hope_cohort_cnv %>%
 print(length(unique(hope_cohort_cnv$Kids_First_Biospecimen_ID)))
 saveRDS(hope_cohort_cnv, file = file.path(output_dir, "Hope-cnv-controlfreec.rds"))
 
-# manifest for msi files
-msi_manifest <- read_tsv(file.path(input_dir, "manifest", "manifest_20230504_084539_msi.tsv"))
-colnames(msi_manifest) <- gsub(" ", "_", colnames(msi_manifest))
-msi_manifest <- msi_manifest %>%
-  dplyr::select(name, Kids_First_Biospecimen_ID, case_id, sample_id) %>%
-  mutate(file_name = gsub('.*/', '', name)) %>%
-  unique()
-
-# filter to BS-ids in histology file only
-msi_manifest <- msi_manifest %>%
-  filter(Kids_First_Biospecimen_ID %in% hist_df$Kids_First_Biospecimen_ID)
-
-# merge msi (n = 73)
-lf <- list.files(path = file.path(input_dir, "msisensor_pro"), pattern = 'msisensor_pro', recursive = T, full.names = T)
-hope_cohort_msi <- lapply(lf, read_tsv)
-hope_cohort_msi <- do.call(rbind, hope_cohort_msi)
-colnames(hope_cohort_msi)[3] <- "Percent"
-hope_cohort_msi$name <- gsub('.*/', '', lf)
-
-# add identifiers 
-hope_cohort_msi <- hope_cohort_msi %>%
-  inner_join(msi_manifest %>% dplyr::select(-c(name)), by = c("name" = "file_name")) %>%
-  dplyr::select(Kids_First_Biospecimen_ID, case_id, sample_id, Total_Number_of_Sites, Number_of_Somatic_Sites, Percent)
-hope_cohort_msi <- unique(hope_cohort_msi)
-print(length(unique(hope_cohort_msi$Kids_First_Biospecimen_ID)))
-saveRDS(hope_cohort_msi, file = file.path(output_dir, "Hope-msi-paired.rds"))
-
 # merge cnv (n = 72)
 # hope_cohort_cnv <- list.files(path = file.path("data", "copy_number"), pattern = "*.cns", recursive = TRUE, full.names = T)
 # hope_cohort_cnv <- sapply(hope_cohort_cnv, FUN = function(x) read_tsv(x), simplify = FALSE)
--- analyses/merge-files/02-create_merged_files_tumor_only.R
+++ analyses/merge-files/02-create_merged_files_tumor_only.R
@@ -85,30 +85,3 @@ hope_cohort_cnv <- hope_cohort_cnv %>%
   filter(Kids_First_Biospecimen_ID %in% hist_df$Kids_First_Biospecimen_ID)
 print(length(unique(hope_cohort_cnv$Kids_First_Biospecimen_ID)))
 saveRDS(hope_cohort_cnv, file = file.path(output_dir, "Hope-cnv-controlfreec-tumor-only.rds"))
-
-# manifest for msi files
-msi_manifest <- read_tsv(file.path(input_dir, "manifest_tumor_only", "manifest_20230504_083829_msi.tsv"))
-colnames(msi_manifest) <- gsub(" ", "_", colnames(msi_manifest))
-msi_manifest <- msi_manifest %>%
-  dplyr::select(name, Kids_First_Biospecimen_ID, case_id, sample_id) %>%
-  mutate(file_name = gsub('.*/', '', name)) %>%
-  unique()
-
-# filter to BS-ids in histology file only
-msi_manifest <- msi_manifest %>%
-  filter(Kids_First_Biospecimen_ID %in% hist_df$Kids_First_Biospecimen_ID)
-
-# merge msi (n = 90)
-lf <- list.files(path = file.path(input_dir, "msisensor_pro_tumor_only"), pattern = 'msisensor_pro', recursive = T, full.names = T)
-hope_cohort_msi <- lapply(lf, read_tsv)
-hope_cohort_msi <- do.call(rbind, hope_cohort_msi)
-colnames(hope_cohort_msi)[3] <- "Percent"
-hope_cohort_msi$name <- gsub('.*/', '', lf)
-
-# add identifiers 
-hope_cohort_msi <- hope_cohort_msi %>%
-  inner_join(msi_manifest %>% dplyr::select(-c(name)), by = c("name" = "file_name")) %>%
-  dplyr::select(Kids_First_Biospecimen_ID, case_id, sample_id, Total_Number_of_Sites, Number_of_Somatic_Sites, Percent)
-hope_cohort_msi <- unique(hope_cohort_msi)
-print(length(unique(hope_cohort_msi$Kids_First_Biospecimen_ID)))
-saveRDS(hope_cohort_msi, file = file.path(output_dir, "Hope-msi-tumor_only.rds"))
