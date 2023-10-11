90758e0739f3e401dae5bcd7cb2fe496d543e75f
komalsrathi <komal.rathi@gmail.com>
2023-10-11 13:26:44 -0400

    update alt analysis

--- analyses/alt-analysis/01-correlation_alt_vs_vars.R
+++ analyses/alt-analysis/01-correlation_alt_vs_vars.R
@@ -1,122 +1,189 @@
-# Function: correlate ALT status to clinical variables like Protein clusters, Age (2 and 3 groups), Gender and TMB
+# Function: correlate ALT status to clinical variables like Protein clusters, Age (2 and 3 groups), Gender and tmb_paired
 
 # load libraries
 suppressPackageStartupMessages({
   library(tidyverse)
   library(ggplot2)
   library(ggpubr)
+  library(ggforce)
 })
 
 # output directory
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
-input_dir <- file.path(root_dir, "analyses", "master-annotation", "results")
+data_dir <- file.path(root_dir, "data", "v1")
 analyses_dir <- file.path(root_dir, "analyses", "alt-analysis")
+input_dir <- file.path(analyses_dir, "input")
 output_dir <- file.path(analyses_dir, "results")
 dir.create(output_dir, showWarnings = F, recursive = T)
+
+# read histologies 
+annot <- read_tsv(file = file.path(data_dir, "Hope-GBM-histologies.tsv"))
+annot <- annot %>% 
+  filter(!is.na(molecular_subtype))
+
+# read MSI paired output 
+msi_paired_output <- read_tsv(file.path("../msi-sensor-analysis/results/msisensor-pro-paired/Hope-msi-paired.tsv")) %>%
+  dplyr::mutate(Type = ifelse(Percent > 3.5, sample_id, "")) %>%
+  dplyr::rename("msi_paired" = "Percent")
+
+# read tmb_paired paired output 
+tmb_paired_paired_output <- read_tsv("../tmb-calculation/results/wgs_paired/snv-mutation-tmb-coding.tsv") %>%
+  dplyr::rename("tmb_paired" = "tmb")
+
+# read ALT status
+alt_status_output <- read_tsv(file.path(output_dir, "alt_status_aya_hgg.tsv")) %>%
+  dplyr::select(sample_id, t_n_telomere_content, ALT_status)
+
+# add proteomics cluster data
+proteomic_data <- read_tsv(file.path(input_dir, "cluster_data_090722.tsv")) %>%
+  dplyr::select(id, rdt.cc)
+
+# add developmental cluster data
+dev_data <- read_tsv(file.path(input_dir, "cluster_data_101922.tsv")) %>%
+  dplyr::select(id, dtt.cc, rdt.name)
+
+# combine all (n = 71)
+output_df <- msi_paired_output %>%
+  inner_join(tmb_paired_paired_output, by = c("Kids_First_Biospecimen_ID" = "Tumor_Sample_Barcode")) %>%
+  inner_join(alt_status_output) %>%
+  left_join(proteomic_data, by = c("sample_id" = "id")) %>%
+  left_join(dev_data, by = c("sample_id" = "id")) %>%
+  inner_join(annot)
+
+# split Age into 2 and 3 groups
+output_df <- output_df %>%
+  mutate(HARMONY_age_class_derived = as.character(HARMONY_age_class_derived)) %>%
+  mutate(age_three_groups = HARMONY_age_class_derived,
+         age_two_groups = ifelse(HARMONY_age_class_derived %in% c("(15,26]", "(26,40]"), "(15,40]", HARMONY_age_class_derived))
 
 # function to compute correlations
 compute_corr <- function(x){
 
   # t_n_telomere_content vs MSI
-  tel_content_vs_msi_corr = cor.test(x = x$t_n_telomere_content, y = x$MSI_Percent)$estimate
-  tel_content_vs_msi_pvalue = cor.test(x = x$t_n_telomere_content, y = x$MSI_Percent)$p.value
+  tel_content_vs_msi_paired_correlation = cor.test(x = x$t_n_telomere_content, y = x$msi_paired)$estimate
+  tel_content_vs_msi_paired_pvalue = cor.test(x = x$t_n_telomere_content, y = x$msi_paired)$p.value
   
-  # t_n_telomere_content vs TMB
-  tel_content_vs_tmb_corr = cor.test(x = x$t_n_telomere_content, y = x$TMB)$estimate
-  tel_content_vs_tmb_pvalue = cor.test(x = x$t_n_telomere_content, y = x$TMB)$p.value
+  # t_n_telomere_content vs tmb_paired
+  tel_content_vs_tmb_paired_correlation = cor.test(x = x$t_n_telomere_content, y = x$tmb_paired)$estimate
+  tel_content_vs_tmb_paired_pvalue = cor.test(x = x$t_n_telomere_content, y = x$tmb_paired)$p.value
   
-  # for ALT vs Age (two groups)
-  age_two_group_pvalue <- chisq.test(x = factor(x$ALT_status), y = factor(x$age_two_groups))$p.value
-  age_two_group_pvalue <- round(age_two_group_pvalue, digits = 3)
+  # ALT status vs Age (two groups)
+  alt_status_vs_age_two_group_chisq_pvalue <- chisq.test(x = factor(x$ALT_status), y = factor(x$age_two_groups))$p.value
+  alt_status_vs_age_two_group_chisq_pvalue <- round(alt_status_vs_age_two_group_chisq_pvalue, digits = 3)
   
-  # for ALT vs Age (three groups)
-  age_three_group_pvalue <- chisq.test(x = factor(x$ALT_status), y = factor(x$age_three_groups))$p.value
-  age_three_group_pvalue <- round(age_three_group_pvalue, digits = 3)
+  # ALT status vs Age (three groups)
+  alt_status_vs_age_three_group_chisq_pvalue <- chisq.test(x = factor(x$ALT_status), y = factor(x$age_three_groups))$p.value
+  alt_status_vs_age_three_group_chisq_pvalue <- round(alt_status_vs_age_three_group_chisq_pvalue, digits = 3)
   
-  # Sex
-  gender_pvalue <- chisq.test(x = factor(x$ALT_status), y = factor(x$Gender))$p.value
-  gender_pvalue <- round(gender_pvalue, digits = 3)
+  # ALT status vs Sex
+  alt_status_vs_gender_chisq_pvalue <- chisq.test(x = factor(x$ALT_status), y = factor(x$HARMONY_Gender))$p.value
+  alt_status_vs_gender_chisq_pvalue <- round(alt_status_vs_gender_chisq_pvalue, digits = 3)
   
-  # Protein cluster
-  rdt_cc_pvalue <- chisq.test(x = factor(x$ALT_status), y = factor(x$rdt.cc))$p.value
-  rdt_cc_pvalue <- round(rdt_cc_pvalue, digits = 3)
+  # ALT status vs Protein cluster
+  alt_status_vs_protein_cluster_chisq_pvalue <- chisq.test(x = factor(x$ALT_status), y = factor(x$rdt.cc))$p.value
+  alt_status_vs_protein_cluster_chisq_pvalue <- round(alt_status_vs_protein_cluster_chisq_pvalue, digits = 3)
   
-  # Protein cluster name
-  rdt_name_pvalue <- chisq.test(x = factor(x$ALT_status), y = factor(x$rdt.name))$p.value
-  rdt_name_pvalue <- round(rdt_name_pvalue, digits = 3)
+  # ALT status vs Protein cluster name
+  alt_status_vs_protein_cluster_name_chisq_pvalue <- chisq.test(x = factor(x$ALT_status), y = factor(x$rdt.name))$p.value
+  alt_status_vs_protein_cluster_name_chisq_pvalue <- round(alt_status_vs_protein_cluster_name_chisq_pvalue, digits = 3)
   
-  # MSI
-  # msi_pvalue <- chisq.test(x = factor(x$ALT_status), y = x$MSI_Percent)$p.value
-  msi_pvalue <- wilcox.test(formula = x$MSI_Percent ~ factor(x$ALT_status))$p.value
-  msi_pvalue <- round(msi_pvalue, digits = 3)
+  # ALT status vs MSI (paired)
+  # msi_pvalue <- chisq.test(x = factor(x$ALT_status), y = x$msi_paired)$p.value
+  alt_status_vs_msi_paired_chisq_pvalue <- wilcox.test(formula = x$msi_paired ~ factor(x$ALT_status))$p.value
+  alt_status_vs_msi_paired_chisq_pvalue <- round(alt_status_vs_msi_paired_chisq_pvalue, digits = 3)
   
-  # TMB
-  # tmb_pvalue <- chisq.test(x = factor(x$ALT_status), y = x$TMB)$p.value
-  tmb_pvalue <- wilcox.test(formula = x$TMB ~ factor(x$ALT_status))$p.value
-  tmb_pvalue <- round(tmb_pvalue, digits = 3)
+  # ALT status vs TMB (paired)
+  # tmb_paired_pvalue <- chisq.test(x = factor(x$ALT_status), y = x$tmb_paired)$p.value
+  alt_status_vs_tmb_paired_chisq_pvalue <- wilcox.test(formula = x$tmb_paired ~ factor(x$ALT_status))$p.value
+  alt_status_vs_tmb_paired_chisq_pvalue <- round(alt_status_vs_tmb_paired_chisq_pvalue, digits = 3)
 
-  output_df <- data.frame(tel_content_vs_msi_corr,
-                          tel_content_vs_msi_pvalue,
-                          tel_content_vs_tmb_corr,
-                          tel_content_vs_tmb_pvalue,
-                          age_two_group_pvalue, age_three_group_pvalue, 
-                          gender_pvalue, 
-                          rdt_cc_pvalue, rdt_name_pvalue,
-                          msi_pvalue, tmb_pvalue)
+  # summarize
+  output_df <- data.frame(tel_content_vs_msi_paired_correlation,
+                          tel_content_vs_msi_paired_pvalue,
+                          tel_content_vs_tmb_paired_correlation,
+                          tel_content_vs_tmb_paired_pvalue,
+                          alt_status_vs_age_two_group_chisq_pvalue, 
+                          alt_status_vs_age_three_group_chisq_pvalue, 
+                          alt_status_vs_gender_chisq_pvalue, 
+                          alt_status_vs_protein_cluster_chisq_pvalue, 
+                          alt_status_vs_protein_cluster_name_chisq_pvalue,
+                          alt_status_vs_msi_paired_chisq_pvalue, 
+                          alt_status_vs_tmb_paired_chisq_pvalue)
   return(output_df)
 }
 
-# master histology file
-hist_df <- read_tsv(file = file.path(input_dir, "master_histology_hope_cohort.tsv"))
-
-# 1) tumor-normal-paired comparisons
-alt_status <- hist_df %>%
-  filter(!is.na(msi_paired)) %>%
-  dplyr::rename("TMB" = "TMB_paired",
-                "MSI_Percent" = "msi_paired")
-df <- compute_corr(x = alt_status)
-rownames(df) <- "Tumor-Normal"
+# 1) correlations
+df <- compute_corr(x = output_df)
+df <- reshape2::melt(df)
 write_tsv(x = df, file = file.path(output_dir, "alt_correlations_paired.tsv"))
 
-# ALT telomere vs TMB scatter plot
-p <- ggplot(alt_status, aes(x = t_n_telomere_content, y = TMB)) +
+# 2) ALT telomere content vs TMB (paired) scatter plot
+p <- ggplot(output_df, aes(x = t_n_telomere_content, y = tmb_paired)) +
   geom_point(pch = 21) +
   theme_pubr() + 
-  xlab("Telomere Content") + ylab("TMB") + ggtitle("Telomere content vs TMB") +
+  xlab("Telomere Content") + ylab("tmb_paired") + ggtitle("Telomere content vs tmb_paired") +
   stat_cor(method = "pearson", color = "red")
 p
 ggsave(plot = p, filename = file.path(output_dir, "alt_content_vs_tmb_paired.png"))
 
-# ALT status vs TMB boxplot
-plot_data <- alt_status %>% 
+# 3) ALT status vs TMB (paired) boxplot
+plot_data <- output_df %>% 
   group_by(ALT_status) %>% 
   mutate(n = n(), ALT_status = paste0(ALT_status, "\n(n = ", n, ")"))
-p <- ggplot(plot_data, aes(x = ALT_status, y = TMB, color = ALT_status)) +
+p <- ggplot(plot_data, aes(x = ALT_status, y = tmb_paired, color = ALT_status)) +
   stat_boxplot(geom ='errorbar', width = 0.2) +
   geom_boxplot(lwd = 0.5, fatten = 0.5, outlier.shape = 1, width = 0.4, outlier.size = 1) +
   ggpubr::theme_pubr(base_size = 10) + ylab("") + 
   stat_compare_means(color = "red", 
-                     label = "p.format",
-                     ref.group = ".all.",
                      size = 4) +
   xlab("") + 
-  ylab("TMB") +
-  ggtitle("ALT Status vs TMB") +
+  ylab("tmb_paired") +
+  ggtitle("ALT Status vs tmb_paired") +
   geom_hline(yintercept = 3.5, linetype = 'dotted', col = 'red') +
   theme(legend.position = "none") 
 ggsave(plot = p, filename = file.path(output_dir, "alt_status_vs_tmb_paired.png"), height = 6, width = 6)
 
-# ALT status vs telomere content
+# 4) ALT status vs telomere content
 p <- ggplot(plot_data, aes(x = ALT_status, y = t_n_telomere_content, color = ALT_status)) +
   stat_boxplot(geom ='errorbar', width = 0.2) +
   geom_boxplot(lwd = 0.5, fatten = 0.5, outlier.shape = 1, width = 0.4, outlier.size = 1) +
   ggpubr::theme_pubr(base_size = 10) + ylab("") + 
   stat_compare_means(color = "red", 
-                     label = "p.format",
                      size = 4) +
   xlab("") + 
-  ylab("TMB") +
+  ylab("tmb_paired") +
   ggtitle("ALT Status vs Telomere Content") +
   geom_hline(yintercept = 3.5, linetype = 'dotted', col = 'red') +
   theme(legend.position = "none") 
 ggsave(plot = p, filename = file.path(output_dir, "alt_status_vs_telomere_content.png"), height = 6, width = 4)
+
+# 5) ALT telomere content vs Age three groups
+plot_df <- output_df %>%
+  group_by(age_three_groups) %>%
+  mutate(n = n(), age_three_groups = paste0(age_three_groups, "\n(n = ", n, ")"))
+plot_df$age_three_groups <- factor(plot_df$age_three_groups,  levels = c("[0,15]\n(n = 42)", "(15,26]\n(n = 20)", "(26,40]\n(n = 8)"))
+p <- ggplot(plot_df, aes(x = age_three_groups, y = t_n_telomere_content, color = age_three_groups)) +
+  geom_boxplot(coef=0) +
+  geom_sina() +
+  ggpubr::theme_pubr(base_size = 10) + ylab("") + 
+  stat_compare_means(color = "red", size = 4, method = "kruskal.test") +
+  xlab("\nHARMONY_age_class_derived") + 
+  ylab("t_n_telomere_content\n") +
+  theme(legend.position = "none") 
+p
+
+# 6) ALT telomere content vs Age two groups 
+# 2 class
+plot_df <- output_df %>%
+  group_by(age_two_groups) %>%
+  mutate(n = n(), age_two_groups = paste0(age_two_groups, "\n(n = ", n, ")"))
+plot_df$age_two_groups <- factor(plot_df$age_two_groups, levels = c("[0,15]\n(n = 42)", "(15,40]\n(n = 28)"))
+q <- ggplot(plot_df, aes(x = age_two_groups, y = t_n_telomere_content, color = age_two_groups)) +
+  geom_boxplot(coef=0) +
+  geom_sina() +
+  ggpubr::theme_pubr(base_size = 10) + ylab("") + 
+  stat_compare_means(color = "red", size = 4, method = "wilcox.test") +
+  xlab("\nHARMONY_age_class_derived") + 
+  ylab("t_n_telomere_content\n") +
+  theme(legend.position = "none") 
+q
--- analyses/alt-analysis/02-alt_vs_age_boxplots.R
+++ analyses/alt-analysis/02-alt_vs_age_boxplots.R
@@ -15,49 +15,13 @@ output_dir <- file.path(analyses_dir, "results")
 dir.create(output_dir, showWarnings = F, recursive = T)
 
 # master histology file
-hist_df <- read_tsv(file = file.path(root_dir, "data", "Hope-GBM-histologies.tsv"))
-alt_status <- read_tsv(file.path(output_dir, "alt_status_aya_hgg.tsv"))
-output_df <- hist_df %>%
+hist_df <- read_tsv(file = file.path(root_dir, "data", "v1", "Hope-GBM-histologies.tsv"))
+alt_status <- read_tsv(file.path("../alt-analysis/results/alt_status_aya_hgg.tsv"))
+tmp <- hist_df %>%
   filter(!is.na(molecular_subtype)) %>%
   inner_join(alt_status) %>%
   dplyr::select(sample_id, experimental_strategy, ALT_status, t_n_telomere_content, HARMONY_age_class_derived) %>%
   group_by(sample_id) %>%
   mutate(experimental_strategy = toString(experimental_strategy)) %>%
   unique()
-output_df <- output_df[grep("WGS", output_df$experimental_strategy),]
-
-# 3 class
-plot_df <- output_df %>%
-  group_by(HARMONY_age_class_derived) %>%
-  mutate(n = n(), HARMONY_age_class_derived = paste0(HARMONY_age_class_derived, "\n(n = ", n, ")"))
-plot_df$HARMONY_age_class_derived <- factor(plot_df$HARMONY_age_class_derived, 
-                                            levels = c("[0,15]\n(n = 43)", "(15,26]\n(n = 20)", "(26,40]\n(n = 8)"))
-p <- ggplot(plot_df, aes(x = HARMONY_age_class_derived, y = t_n_telomere_content, color = HARMONY_age_class_derived)) +
-  # geom_violin() +
-  geom_boxplot(coef=0) +
-  geom_sina() +
-  ggpubr::theme_pubr(base_size = 10) + ylab("") + 
-  stat_compare_means(color = "red", size = 4, method = "kruskal.test") +
-  xlab("\nHARMONY_age_class_derived") + 
-  ylab("t_n_telomere_content\n") +
-  theme(legend.position = "none") 
-p
-
-# 2 class
-plot_df <- output_df %>%
-  mutate(HARMONY_age_class_derived = as.character(HARMONY_age_class_derived)) %>%
-  mutate(HARMONY_age_class_derived = ifelse(HARMONY_age_class_derived %in% c("(15,26]", "(26,40]"), "(15,40]", HARMONY_age_class_derived))
-plot_df <- plot_df %>%
-  group_by(HARMONY_age_class_derived) %>%
-  mutate(n = n(), HARMONY_age_class_derived = paste0(HARMONY_age_class_derived, "\n(n = ", n, ")"))
-plot_df$HARMONY_age_class_derived <- factor(plot_df$HARMONY_age_class_derived, levels = c("[0,15]\n(n = 43)", "(15,40]\n(n = 28)"))
-q <- ggplot(plot_df, aes(x = HARMONY_age_class_derived, y = t_n_telomere_content, color = HARMONY_age_class_derived)) +
-  # geom_violin() +
-  geom_boxplot(coef=0) +
-  geom_sina() +
-  ggpubr::theme_pubr(base_size = 10) + ylab("") + 
-  stat_compare_means(color = "red", size = 4, method = "wilcox.test") +
-  xlab("\nHARMONY_age_class_derived") + 
-  ylab("t_n_telomere_content\n") +
-  theme(legend.position = "none") 
-q
+tmp <- tmp[grep("WGS", tmp$experimental_strategy),]
