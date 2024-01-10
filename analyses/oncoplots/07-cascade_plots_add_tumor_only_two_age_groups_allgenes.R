40e163a1eddcab03039b2ec1486654423ed34e45
komalsrathi <komal.rathi@gmail.com>
2024-01-10 10:10:09 -0500

    update cascade plots

--- analyses/oncoplots/07-cascade_plots_add_tumor_only_two_age_groups_allgenes.R
+++ analyses/oncoplots/07-cascade_plots_add_tumor_only_two_age_groups_allgenes.R
@@ -42,11 +42,12 @@ alter_fun = list(
 )
 col = c("Mutation" = "black")
 
-# read annotation and TMB info
+# read annotation 
 annot_info <- read.delim(file.path(input_dir, "annotation_add_tumor_only.txt"), header = TRUE, check.names = TRUE)
 annot_info <- annot_info %>%
   filter(Sample %in% colnames(mat),
          Sequencing_Experiment != "RNA-Seq") %>%
+  dplyr::select(-c(CNS_region)) %>%
   remove_rownames() %>%
   column_to_rownames('Sample') %>%
   as.data.frame()
@@ -54,18 +55,36 @@ samples_to_use <- intersect(rownames(annot_info), colnames(mat))
 mat <- mat[,samples_to_use]
 annot_info <- annot_info[samples_to_use,]
 
-# two age groups
+# convert to two age groups
 annot_info <- annot_info %>%
   mutate(Age = as.character(Age)) %>%
   mutate(Age = ifelse(Age %in% c("(15,26]", "(26,40]"), "(15,40]", Age))
-
-# annotation 1
-col_fun_tmb = colorRamp2(c(0, max(annot_info$TMB, na.rm = T)), c("white", "magenta3"))
 annot_info$Age <- factor(annot_info$Age, levels = c("[0,15]", "(15,40]"))
-annot_info$Molecular_Subtype <- as.character(annot_info$Molecular_Subtype)
-annot_info$Cancer_Group <- as.character(annot_info$Cancer_Group)
 
-ha = HeatmapAnnotation(df = annot_info %>% dplyr::select(-c(Sequencing_Experiment)), col = list(
+# color code for TMB
+col_fun_tmb = colorRamp2(c(0, max(annot_info$TMB, na.rm = T)), c("white", "magenta3"))
+
+# Cancer_Group should be PXA where Molecular_Subtype is PXA
+annot_info <- annot_info %>%
+  mutate(Cancer_Group = ifelse(Molecular_Subtype == "PXA", "PXA", Cancer_Group))
+
+# long names for Cancer_Group
+annot_info <- annot_info %>%
+  mutate(Cancer_Group = case_when(Cancer_Group == "DHG" ~ "(DHG) Diffuse Hemispheric Glioma",
+                                  Cancer_Group == "DMG" ~ "(DMG) Diffuse Midline Glioma",
+                                  Cancer_Group == "HGG" ~ "(HGG) High Grade Glioma (not otherwise specified)",
+                                  Cancer_Group == "IHG" ~ "(IHG) Infantile High Grade Glioma",
+                                  Cancer_Group == "PXA" ~ "(PXA) Pleomorphic Xanthoastrocytoma"))
+
+# replace NA values with "Flag"
+annot_info$Molecular_Subtype[is.na(annot_info$Molecular_Subtype)] <- "Flag"
+annot_info$Cancer_Group[is.na(annot_info$Cancer_Group)] <- "Flag"
+
+# remove underscore from column names
+colnames(annot_info) <- gsub("_", " ", colnames(annot_info))
+
+# oncoprint annotation color code
+ha = HeatmapAnnotation(df = annot_info %>% dplyr::select(-c(`Sequencing Experiment`)), col = list(
   TMB = col_fun_tmb,
   Diagnosis = c("High-grade glioma/astrocytoma (WHO grade III/IV)" = "lightseagreen",
                 "Diffuse Midline Glioma (WHO grade III/IV)" = "darkgreen",
@@ -73,37 +92,38 @@ ha = HeatmapAnnotation(df = annot_info %>% dplyr::select(-c(Sequencing_Experimen
                 "Astrocytoma (WHO grade III/IV)" = "#5fff57", 
                 "Glioblastoma (WHO grade IV)" = "#f268d6",
                 "Pleomorphic xanthoastrocytoma (WHO grade II/III)" = "#005082"),
-  Molecular_Subtype = c("DMG, H3 K28" = "#053061",
-                        "DHG, H3 G35, TP53" = "#A6761D",
-                        "HGG, H3 wildtype" = "#4393c3",
-                        "HGG, H3 wildtype, TP53" = "darkgreen",
-                        "DMG, H3 K28, TP53" = "#BC80BD",
-                        "HGG, IDH, TP53" = "#FFFF99",
-                        "IHG, NTRK-altered, TP53"  = "#E7298A",
-                        "IHG, NTRK-altered" = "#f4a582",
-                        "IHG, ROS1-altered" = "#d6604d",
-                        "IHG, ALK-altered" = "#E31A1C",
-                        "PXA" = "#67001f",
-                        "HGG, IDH" = "#B3DE69",
-                        "NA" = "#f1f1f1"),
-  Diagnosis_Type = c("Initial CNS Tumor" = "#cee397",
-                     "Progressive" = "#827397",
-                     "Recurrence" = "#363062",
-                     "Second Malignancy" = "#005082"),
-  Tumor_Location = c("Cortical" = "#D4806C",
-                     "Other/Multiple locations/NOS" = "#7C8F97",
-                     "Midline" = "#344C68",
-                     "Cerebellar" = "#94004C"),
-  CNS_region = c("Posterior fossa" = "#D4806C",
-                 "Other" = "#7C8F97",
-                 "Midline" = "#344C68",
-                 "Hemispheric" = "#94004C",
-                 "Mixed" = "darkgreen"),
-  Cancer_Group = c("DMG" = "#053061",
-                   "DHG" = "#A6761D",
-                   "HGG" = "#4393c3",
-                   "IHG" = "#E7298A",
-                   "NA" = "#f1f1f1"),
+  `Molecular Subtype` = c("DMG, H3 K28" = "#053061",
+                          "DHG, H3 G35, TP53" = "#A6761D",
+                          "HGG, H3 wildtype" = "#4393c3",
+                          "HGG, H3 wildtype, TP53" = "darkgreen",
+                          "DMG, H3 K28, TP53" = "#BC80BD",
+                          "HGG, IDH, TP53" = "#FFFF99",
+                          "IHG, NTRK-altered, TP53"  = "#E7298A",
+                          "IHG, NTRK-altered" = "#f4a582",
+                          "IHG, ROS1-altered" = "#d6604d",
+                          "IHG, ALK-altered" = "#E31A1C",
+                          "PXA" = "#67001f",
+                          "HGG, IDH" = "#B3DE69",
+                          "Flag" = "#f1f1f1"),
+  `Diagnosis Type` = c("Initial CNS Tumor" = "#cee397",
+                       "Progressive" = "#827397",
+                       "Recurrence" = "#363062",
+                       "Second Malignancy" = "#005082"),
+  `Tumor Location` = c("Cortical" = "#D4806C",
+                       "Other/Multiple locations/NOS" = "#7C8F97",
+                       "Midline" = "#344C68",
+                       "Cerebellar" = "#94004C"),
+  `CNS region` = c("Posterior fossa" = "#D4806C",
+                   "Other" = "#7C8F97",
+                   "Midline" = "#344C68",
+                   "Hemispheric" = "#94004C",
+                   "Mixed" = "darkgreen"),
+  `Cancer Group` = c("(DMG) Diffuse Midline Glioma" = "#053061",
+                     "(DHG) Diffuse Hemispheric Glioma" = "#A6761D",
+                     "(HGG) High Grade Glioma (not otherwise specified)" = "#4393c3",
+                     "(IHG) Infantile High Grade Glioma" = "#E7298A",
+                     "(PXA) Pleomorphic Xanthoastrocytoma" = "#67001f",
+                     "Flag" = "#f1f1f1"),
   Sex = c("Male" = "#0707CF",
           "Female" = "#CC0303"),
   Age = c("[0,15]" = "#C7E9C0",
